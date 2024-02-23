aabb149c6a3054ffb94a3904bf9145a12f077c4d
Holberton1990 <135588172+Holberton1990@users.noreply.github.com>
2024-02-23 13:25:55 +0100

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0, extend your Python
-script to export data in the JSON format
+Model to make a request to an
+API and retrieve data
 """
 
 
@@ -13,12 +13,12 @@ from sys import argv
 if __name__ == "__main__":
     URL = "https://jsonplaceholder.typicode.com/"
     user_id = argv[1]
-    data = requests.get(f"{URL}users/{argv[1]}")
-    data = data.json()
-    user_name = data['username']
+    res = requests.get(f"{URL}users/{argv[1]}")
+    res = res.json()
+    user_name = res['username']
 
-    data = requests.get(f"{URL}todos")
-    all_todos = data.json()
+    res = requests.get(f"{URL}todos")
+    all_todos = res.json()
     user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
 
     user_dict = []
@@ -28,7 +28,7 @@ if __name__ == "__main__":
                           "completed": todo['completed'],
                           "username": user_name})
 
-    json_obj = {f"{user_id}": user_dict}
+    json_object = {f"{user_id}": user_dict}
 
-    with open(f"{user_id}.json", 'w') as f:
-        json.dump(json_obj, f)
+    with open(f"{user_id}.json", 'w') as file:
+        json.dump(json_object, file)
